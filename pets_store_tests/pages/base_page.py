e580b2e0ffdf3b009c0c42972b587ed4f5d8016b
BohdanObruch <BohdanObruch@users.noreply.github.com>
2024-10-23 17:02:39 +0000

    deploy: c11ef8746015605725a44a5f68e5efb4badd347d

--- pets_store_tests/pages/base_page.py
+++
@@ -1,7 +0,0 @@
-class BasePage:
-    def __init__(self, base_url):
-        self.base_url = base_url
-
-    @staticmethod
-    def validate(model, response):
-        return model(**response.json())
--- pets_store_tests/test_data/store.py
+++
@@ -1,8 +0,0 @@
-place_order = {
-        "id": 1,
-        "petId": 1,
-        "quantity": 1,
-        "shipDate": "2023-10-01T00:00:00.000Z",
-        "status": "placed",
-        "complete": True
-    }
